15122a0a9f8ce75fc7965d360dd5ee4cc5af1edf
Regggio <111200473+Regggio@users.noreply.github.com>
2025-04-27 20:25:21 -0300

    Update Creacion_Executes_SP.sql

--- SQL/Creacion_Executes_SP.sql
+++ SQL/Creacion_Executes_SP.sql
@@ -1,9 +1,21 @@
-EXECUTE grupo3.ReporteTrimestral 1, 2024
+--EJECUCION DE LOS SP DE REGISTROS
 
-EXECUTE grupo3.ReporteMensual 3,2024
+GO
+USE Com2900G03
+GO
 
-EXECUTE grupo3.ReporteRango '2024,01,01','2024,12,31'
+EXECUTE grupo3.ReporteTrimestral 1, 2019
 
-EXECUTE grupo3.ReporteRangoSucursal '2024,01,01','2024,12,31'
+EXECUTE grupo3.ReporteMensual 3,2019
 
-EXECUTE grupo3.ReporteSemanal 01,2024
+EXECUTE grupo3.ReporteRango '2019-01-01', '2019-12-31'
+
+EXECUTE grupo3.ReporteRangoSucursal '2019-01-01', '2019-12-31'
+
+EXECUTE grupo3.ReporteSemanal 01,2019
+
+EXECUTE grupo3.ReporteMenosVendidos 01, 2019
+
+EXECUTE grupo3.VentasFechaSucursal '2019-01-01', 1
+
+EXECUTE grupo3.ReporteGeneral
